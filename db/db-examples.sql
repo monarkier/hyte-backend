0ec191b022c39d146dce7403fe0ebb0fc0ea7d2e
monarkier <142782233+monarkier@users.noreply.github.com>
2025-02-23 10:43:04 +0200

    Add database design and sql script

---
+++ db/db-examples.sql
@@ -0,0 +1,88 @@
+-- Windows: MySQL Client start-valikko
+-- Mysql client käynnistys komentoriviltä
+-- mysql -u root -pMUNSALASANA
+
+-- Tämän skriptin suorittaminen (opettajan kone) sql clientissa:
+-- source /Users/mattpe/code/hyte/server-example-25/db/db-examples.sql
+
+DROP DATABASE IF EXISTS HealthDiary;
+CREATE DATABASE HealthDiary;
+USE HealthDiary;
+
+-- Create a table for users
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    fk_user_role INT DEFAULT 2;
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (fk_user_role) REFERENCES Roles(id)
+);
+
+-- Create a table for diary entries
+CREATE TABLE DiaryEntries (
+    entry_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    entry_date DATE NOT NULL,
+    mood VARCHAR(50),
+    weight DECIMAL(5,2),
+    sleep_hours INT,
+    notes TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+-- Create table for roles
+CREATE TABLE Roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+-- Create table for comments
+CREATE TABLE Comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    diary_entry_id INT NOT NULL,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (diary_entry_id) REFERENCES DiaryEntries(entry_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+-- ALTER example, adding a new column to existing table
+ALTER TABLE Users ADD COLUMN user_level VARCHAR(10) DEFAULT 'regular';
+
+-------------------
+-- insert test data
+-------------------
+
+-- Inserting a single record, without specifying column names
+INSERT INTO Users
+  VALUES (1, 'johndoe', 'temp-pw-1', 'johndoe@example.com', '2024-01-02 10:00:00', 'regular');
+
+-- Iserting multiple user rows at once (default values like created_at are inserted without need to specify them)
+INSERT INTO Users (username, password, email, user_level) VALUES
+  ('janedoe', 'temp-pw-2', 'janedoe@example.com', 'admin'),
+  ('mike_smith', 'temp-pw-3', 'mike@example.com', 'moderator');
+
+-- Example when FK constraint fails (if user_id 15 does not exist) -> changed to 3
+INSERT INTO DiaryEntries (user_id, entry_date, mood, weight, sleep_hours, notes, created_at) VALUES
+  (3, '2024-01-10', 'Happy', 70.5, 8, 'Had a great day, felt energetic', '2024-01-10 20:00:00');
+
+-- Inserting multiple diary entries
+INSERT INTO DiaryEntries (user_id, entry_date, mood, weight, sleep_hours, notes, created_at) VALUES
+  (1, '2024-01-10', 'Happy', 70.5, 8, 'Had a great day, felt energetic', '2024-01-10 20:00:00'),
+  (1, '2024-01-11', 'Tired', 70.2, 6, 'Long day at work, need rest', '2024-01-11 20:00:00'),
+  (2, '2024-01-10', 'Stressed', 65.0, 7, 'Busy day, a bit stressed out', '2024-01-10 21:00:00');
+
+-- Inserting roles
+INSERT INTO Roles (name) VALUES
+('admin'),
+('user');
+
+-- inserting comments
+INSERT INTO Comments (diary_entry_id, user_id, content, created_at) VALUES
+(1, 2, 'That sounds awesome!', NOW()),
+(1, 3, 'great job!', NOW()),
+(2, 1, 'What exercises did you do?', NOW());
